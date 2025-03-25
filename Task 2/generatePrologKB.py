ed5d7289dd538855ebba4c79647efd1042863984
vincent3477 <vincent.siu.0101@gmail.com>
2025-03-25 15:15:05 -0700

    add comments and format for code

--- Task 2/generatePrologKB.py
+++ Task 2/generatePrologKB.py
@@ -2,11 +2,12 @@ from openai import OpenAI
 import janus_swi as janus
 client = OpenAI()
 
+#Ask user for input problem and where to save the generated .pl file.
 input_prompt = input("Type in your problem here: ")
 fileName = input("Where do you want to save the file?")
 fileName = fileName + ".pl"
 
-
+#Create a GPT completion instance with the OpenAI API, where GPT must translate the input_prompt to prolog code.
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
@@ -17,12 +18,14 @@ completion = client.chat.completions.create(
     logprobs=True
 )
 
-
+#store the output.
 pl_output = completion.choices[0].message.content
 
+#write all of the lines (except the headers generated) into the said file.
 with open(fileName, "a") as file:
     file.write(pl_output[9:len(pl_output)-3]) #strip the headers.
 
+#Use janus to connsult the filename ensuring the .pl file generated is valid.
 janus.consult(fileName)
 
-print("If you DO NOT see any errors, that means the prolog file was able to compile successfully. Program finished.")
+print("Program finished.")
--- Task 4/GPTSymbolicSolver.py
+++ Task 4/GPTSymbolicSolver.py
@@ -2,11 +2,13 @@ from openai import OpenAI
 import janus_swi as janus
 client = OpenAI()
 
+#Ask user for input problem and where to save the generated .pl file.
 input_prompt = input("Type in your problem here: ")
 file_prompt  = input("Where do you want to save the file?")
 fileName = file_prompt + ".pl"
 
-
+#Create a GPT completion instance with the OpenAI API, where GPT must translate the input_prompt to prolog code.
+#The last line of the generated code will the conclusion that is to be drawn.
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
@@ -17,14 +19,17 @@ completion = client.chat.completions.create(
     logprobs=True
 )
 
-
+#store the output.
 pl_output = completion.choices[0].message.content
 
+#write all of the lines (except the headers generated) into the said file.
 with open(fileName, "a") as file:
     file.write(pl_output[9:len(pl_output)-3]) #strip the headers.
 
+#Use janus to connsult the filename ensuring the .pl file generated is valid.
 janus.consult(fileName)
 
+#Get the last line of the file, which will be the conclusion made.
 last_line = ""
 with open(fileName, "r") as file:
     lines = file.readlines()
@@ -32,7 +37,8 @@ with open(fileName, "r") as file:
         last_line = lines[-1].rstrip('\n')
 
 print(last_line)
- 
+
+#query that line (the conclusion), and return whether or not it's valid (true or false). 
 result = janus.query_once(last_line)
 print(result)
 
