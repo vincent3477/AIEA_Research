894cf0a417e728d381569c1493fedee70b4fcfab
vincent3477 <vincent.siu.0101@gmail.com>
2026-03-04 00:39:03 -0800

    forgot to add final changes to eval agents

--- PersonaRag/evaluate_agent.py
+++ PersonaRag/evaluate_agent.py
@@ -5,9 +5,16 @@ from ragas.llms import LangchainLLMWrapper
 from ragas.metrics import (answer_relevancy, faithfulness, context_recall, context_precision, answer_correctness)
 from ragas import evaluate
 from datasets import load_dataset, Dataset
+from langchain.schema import SystemMessage, HumanMessage
 
+## a small program for testing the personarag pipeline.
 
 def test_agent():
+
+
+    llm_judge = ChatOpenAI(model="gpt-4o")
+
+    correctness_measurement = []
 
     agent = persona_rag()
     
@@ -15,31 +22,54 @@ def test_agent():
 
     qa_set = []
 
-    limit = 24
+    limit = 11
     i = 0
 
 
     
 
-    llm_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-    evaluator_llm = LangchainLLMWrapper(llm_model)  
 
     print(len(dataset["test"]))
 
     
     for entry in dataset["test"]:
         query = entry["question"]
-        answers = entry["answers"]
-        answer, context_articles = agent.ask_question(query)
+        reference = entry["answers"]
+
+        try:
+            answer, context_articles = agent.ask_question(query)
+        except openai.RateLimitError as e:
+            print(f"error trying to query {e}, skipping to next")
+            i += 1
+            continue
+
+        
         #question -> string
         #contexts -> list of strings
         #answer -> string
         #reference -> string
-        qa = {"question": query, "contexts": context_articles, "answer": answer, "reference": ";".join(answers)}
+
+        
 
         for a in range(len(context_articles)):
             nt = "".join(c for c in context_articles[a] if c.isprintable())
             context_articles[a] = nt
+
+    
+        
+
+        system_instructions = "You are an expert at evaluating whether or not an answer is correct."
+
+        eval_prompt = f"Given the following \n Question: \"{query}\" \n Retrieved Articles: \"{context_articles}\" \n Model answer: \"{answer}\" \n Reference answer(s): \"{reference}\", evaluate is the model answer correct given the question and the reference. Correct answers in the reference are separated by \";\". When  the Reference Answers field has multiple answers, you can deduct points if and only if the the context of the query require some or all of of the multiple responses. Only output a json formatted exactly as shown in the following {{Evaluation: <put evaluation here>, Score: 0.89}}"
+
+        messages = [SystemMessage(content = system_instructions), HumanMessage(content = eval_prompt)]
+
+        accuracy_results = llm_judge.invoke(messages).content
+
+        correctness_measurement.append(accuracy_results)
+
+        qa = {"question": query, "contexts": context_articles, "answer": answer, "reference": "; ".join(reference)}
+
 
         qa_set.append(qa)
 
@@ -50,13 +80,17 @@ def test_agent():
 
     eval_dataset = Dataset.from_list(qa_set)
 
-    result = evaluate(eval_dataset, metrics = [context_precision, context_recall, faithfulness, answer_relevancy, answer_correctness])
+    result = evaluate(eval_dataset, metrics = [context_precision, context_recall, faithfulness, answer_relevancy])
 
-    
+    print("#### correctness results #####")
+    for i in correctness_measurement:
+        print(i)
+        print("\n")
 
     results_df = result.to_pandas()
 
-    results_df.to_csv('results.csv', index = False)
+    results_df.to_csv('results4.csv', index = False)
+
     print(results_df.head())
 
     results_df
